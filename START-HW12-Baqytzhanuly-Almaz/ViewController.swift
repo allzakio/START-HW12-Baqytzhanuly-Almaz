d728d3822c5c8393ee25b2914777803e94a5a582
allz <bakhytzhanov.7@gmail.com>
2023-06-23 05:59:49 +0600

    Add actions and properties

--- START-HW12-Baqytzhanuly-Almaz/ViewController.swift
+++ START-HW12-Baqytzhanuly-Almaz/ViewController.swift
@@ -11,6 +11,14 @@ import SnapKit
 class ViewController: UIViewController {
     
     // MARK: - Properties
+    private var isStarted: Bool = false
+    private var isWorkTime: Bool = true
+    private let workDuration: TimeInterval = 4
+    private let breakDuration: TimeInterval = 2
+    private var timeRemaining: TimeInterval = 0
+    private var totalDuration: TimeInterval = 0
+    private var timer: Timer = Timer()
+    private var currentTimeForLabel: TimeInterval = 0
     private let circleRadius: CGFloat = 300
     private let redColor: UIColor = UIColor(red: 0.9895820022, green: 0.5559109449, blue: 0.5085786581, alpha: 1)
     private let greenColor: UIColor = UIColor(red: 0.4070520401, green: 0.7629214525, blue: 0.6445431113, alpha: 1)
@@ -114,6 +122,66 @@ class ViewController: UIViewController {
             make.centerX.equalToSuperview()
             make.bottom.equalTo(progressBarView.snp.bottom).offset(-50)
             make.width.height.equalTo(40)
+        }
+    }
+    
+    
+    // MARK: - Actions
+    
+    @objc private func startButtonTapped() {
+        if isStarted {
+            stopTimer()
+            if isWorkTime {
+                startButton.setImage(UIImage(named: "red_play_icon"), for: .normal)
+            } else {
+                startButton.setImage(UIImage(named: "green_play_icon"), for: .normal)
+            }
+        } else {
+            startTimer()
+            startButton.setImage(UIImage(named: "stop_icon"), for: .normal)
+        }
+    }
+    
+    private func startTimer() {
+        if timeRemaining <= 0 {
+            timeRemaining = isWorkTime ? workDuration : breakDuration
+            totalDuration = isWorkTime ? workDuration : breakDuration
+        }
+
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        isStarted = true
+    }
+    
+    private func stopTimer() {
+        timer.invalidate()
+        isStarted = false
+    }
+    
+    @objc private func updateTimer() {
+        currentTimeForLabel = timeRemaining
+        timeRemaining -= 1
+        updateTimerLabel()
+        updateUI()
+
+        if timeRemaining <= 0 {
+            stopTimer()
+            isWorkTime = !isWorkTime
+            startTimer()
+        }
+    }
+    
+    private func updateTimerLabel() {
+        let seconds = Int(currentTimeForLabel) % 60
+        timerLabel.text = "00:\(String(format: "%02d", seconds))"
+    }
+    
+    private func updateUI() {
+        if isWorkTime {
+            progressLayer.strokeColor = redColor.cgColor
+            timerLabel.textColor = redColor
+        } else {
+            progressLayer.strokeColor = greenColor.cgColor
+            timerLabel.textColor = greenColor
         }
     }
 }
